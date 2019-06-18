c68fe918cef79d71cfd01b699ad5bcec443855c7
Aaron <aaron@ibbur.com>
2019-06-17 22:15:14 -0500

    PCC 2019617

--- hello_world2.py
+++ hello_world2.py
@@ -1,3 +1,18 @@
     # Every variable holds a value which is the information associated with the variable.
 message = "Hello Python world!"
 print(message)
+
+    # Variables can be changed at any time.
+message = "Hello Python Crash Course world!"
+print(message)
+
+    # Variable rules
+    # 1) Variable names can contain only letters, numbers, and underscores. The can start with a letter or an underscore, but not with a number
+    # 2) Spaces are not allowed in variable names.
+    # 3) Avoid using Python keywords and function names as variable names.
+    # 4) Variable names should be short but descriptive
+    # 5) Be careful when using the lowercase letter l and the upper case letter O because they can be confused with the numbers 1 and 0.
+
+message = "Hello Python Crash Course reader!"
+print(mesage)
+    # A traceback is a record of where the interpreter ran into trouble when trying to execute your code.
