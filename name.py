3e71acfc43df6d9449a02d45fa063c0da250e740
Aaron <aaron@ibbur.com>
2019-06-18 16:38:47 -0500

    20196-18 PCC

---
+++ name.py
@@ -0,0 +1,8 @@
+name = "ada lovelace"
+print(name.title())
+
+    # A method (i.e., title) is an action that Python can perform on a piece of data. The (.) after name in name.title() tells Python to make the title() method act on the variable name. Every method is followed by a set of parentheses, because methods often need additional information to do their work.
+    # title() displays each word in titlecase, where each word begins with a capital letter.
+
+print(name.upper())
+print(name.lower())
