e6eae20b3a1aae58ee87cd1fb4fc9a24519e6aa4
Aaron <aaron@ibbur.com>
2019-06-21 20:24:40 -0500

    PCC 2019-6-21

---
+++ ex.py
@@ -0,0 +1,4 @@
+motorcycles = ['honda','yamaha','suzuki']
+print(motorcycles)
+motorcycles.pop()
+print(motorcycles)
--- motorcycles.py
+++ motorcycles.py
@@ -50,10 +50,38 @@ motorcycles.insert(1, 'yamaha')
 print(motorcycles)
 
     # Here we'll learn to use the pop method
+    # This first line pops the item out and stores the popped item in a variable.
+    # The pop method removes the last item in the list.
 popped_motorcycles = motorcycles.pop()
 print(motorcycles)
 print(popped_motorcycles)
 
+print("Put the popped item back in. ---")
     # Put the popped item back
 motorcycles.insert(2, 'suzuki')
+print(motorcycles)
+
+last_owned = motorcycles.pop()
+print ("The last motorcycle I owned was a " + last_owned.title() + ".")
+print(motorcycles)
+
+print("Put the popped item back in... again. --- ")
+motorcycles.insert(2, 'susuki')
+print(motorcycles)
+
+    # You can use pop() to remove an item in a list at any positing by including the index of the item you want to remove in parentheses.
+
+first_owned = motorcycles.pop(0)
+print ('The first motorcycle I owned was a ' + first_owned.title() + '.')
+print(motorcycles)
+
+print("Put the popped item back in. --- ")
+motorcycles.insert(0, 'honda')
+print(motorcycles)
+
+print("Add ducati back in for the exercise.")
+motorcycles.append('ducati')
+
+print(motorcycles)
+motorcycles.remove('ducati')
 print(motorcycles)
