ae99d694cc44b7194c1530d436d63d15e7d54c27
Aaron-Laptop <aaron@ibbur.com>
2019-06-23 22:42:38 -0500

    2019-6-23 PCC

---
+++ magicians.py
@@ -0,0 +1,9 @@
+# Aaron Frankel 2019-6-23
+# The purpose of this file is to work with loops
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+print("Thank you, everyone. That was a great magic show!")
