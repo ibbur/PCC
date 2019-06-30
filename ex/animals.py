140ac66afb246b8463fd1062335aafe5b9733bcd
Aaron-Laptop <aaron@ibbur.com>
2019-06-29 19:11:53 -0500

    2019-6-29 PCC

---
+++ ex/animals.py
@@ -0,0 +1,8 @@
+# Aaron Frankel 2019-6-26
+# Playing with lists and for loops
+
+animals = ['squirrels','weimaraners','otters']
+for animal in animals:
+    print("\n" + animal)
+    print(animal.title() + " are silly creatures.")
+print("\nOnly " + animals[1].title() + " make good pets.")
---
+++ ex/pizza.py
@@ -0,0 +1,9 @@
+# Aaron Frankel 2019-6-26
+
+pizzas = ['pepporoni','deep-dish','starburst']
+for pizza in pizzas:
+    print(pizza)
+
+for pizza in pizzas:
+    print("I like " + pizza + " pizza.")
+print("I really love pizza.")
