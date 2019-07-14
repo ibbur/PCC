c4d77bf1e9385e2d299d6433128961e8e412455d
Aaron-Laptop <aaron@ibbur.com>
2019-07-13 20:13:00 -0500

    2019-7-13

---
+++ toppings.py
@@ -0,0 +1,10 @@
+# Aaron Frankel 2019-7-6
+# The purpose of this file is to learn and practise inequalities
+
+requested_toppings = 'mushrooms'
+
+if requested_toppings != 'anchovies':
+    print("Hold the anchovies!")
+
+age = 18
+print(age == 18)
